f97606f93ff76a93ed9200e8c1c20590e5ce2835
ut4k <aquarium011@gmail.com>
2023-11-20 00:55:42 +0900

    wip frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import TasteSelector from "./components/TasteSelector.vue";
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>コーヒー診断</h1>
+    </div>
+    <div>
+      <p>
+        好みを選んで調整したら、「診断する！」を押してね。君好みのコーヒーを教えるよ。
+      </p>
+    </div>
+    <div>
+      <TasteSelector title="コク" min="弱い" max="強い" />
+      <TasteSelector title="香り" min="弱い" max="強い" />
+      <TasteSelector title="苦味" min="弱い" max="強い" />
+      <TasteSelector title="酸味" min="弱い" max="強い" />
+    </div>
+    <div>
+      <button type="button">診断する！</button>
+    </div>
+    <div id="result"></div>
+  </main>
+</template>
+
+<style>
+main {
+  width: 600px;
+  margin: 0 auto;
+}
+
+datalist {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  writing-mode: vertical-lr;
+  width: 200px;
+}
+
+option {
+  padding: 0;
+}
+
+input[type="range"] {
+  width: 200px;
+  margin: 0;
+}
+</style>
+
+<script>
+export default {
+  mounted() {},
+  methods: {},
+};
+</script>
---
+++ frontend/src/components/TasteSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tasteSelector">
+    <label for="temp">{{ title }}</label
+    ><br />
+    <input type="range" list="values" step="25" />
+    <datalist id="values">
+      <option value="0" :label="min"></option>
+      <option value="25"></option>
+      <option value="50"></option>
+      <option value="75"></option>
+      <option value="100" :label="max"></option>
+    </datalist>
+  </div>
+</template>
+
+<style>
+.tasteSelector {
+  margin: 20px 0;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    title: String,
+    min: String,
+    max: String,
+  },
+};
+</script>
